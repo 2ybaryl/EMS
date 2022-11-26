cef999291a30cd242e3900aa45a18c2cac8a8e4d
iamkrati22 <iamkrati22@gmail.com>
2022-11-26 22:00:04 +0530

    Changes in Header & Footer files

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,9 +1,31 @@
 import React from "react";
+import './stylesheets/Footer.css'
 class Footer extends React.Component {
     render() {
         return (
             <footer id="footer">
-                <h2 id="site-name"><a href="/">Footer</a></h2>
+                <div className="logo">SUPERON</div>
+                <div className="sub-header">
+                    <ul className="sub-nav">
+                        <li>Home</li>
+                        <li>Order Tracking</li>
+                        <li>Target</li>
+                        <li>Attendance</li>
+                        <li>Daily Working</li>
+                        <li>Weekly Plan</li>
+                        <li>Request</li>
+                    </ul>
+                </div>
+                <div className="sub-footer">
+                    <div className="copyright">	&#169; 2022 Superon Schweisstechnik India Ltd. All Rights Reserved</div>
+                    <div class = "sub-footer-nav">
+                        <ul>
+                            <li>Help</li>
+                            <li>Privacy Policy</li>
+                            <li>Terms & Conditions</li>
+                        </ul>
+                    </div>
+                </div>
             </footer>
         );
     }
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,12 +1,28 @@
 import React from "react";
 import './stylesheets/Header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
 class Header extends React.Component {
     render() {
         return (
             <header id="header">
                 <div className="main-header">
-                    <div className="logo">Logo</div>
-                    <div className="Sub-text">Hello <br></br> Deepak Kumar</div>
+                    <div className="logo">SUPERON</div>
+                    <div className="Sub-text">Hello <br></br><span>Deepak Kumar</span> </div>
+                    <div className= "search-box">
+                        <input type = "text" placeholder="Search here..."></input>
+                        <button><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+                    </div>
+                    <div className = "main-nav">
+                        <ul>
+                            <li>My Account &#9013;</li>
+                            <li>More &#9013;</li>
+                        </ul>
+                    </div>
+                    <div class = "notifications">
+                        <FontAwesomeIcon icon={faBell} />
+                    </div>
                 </div>
                 <div className="sub-header">
                     <ul className="sub-nav">
